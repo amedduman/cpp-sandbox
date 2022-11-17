96ffcb04e07c8081d17c4209ab987ce3421316ba
amedduman <amedduman@protonmail.com>
2022-11-17 21:46:48 +0300

    Pointers short explanation

--- main.cpp
+++ main.cpp
@@ -2,16 +2,14 @@
 
 int main()
 {
-    int i = 0;
-    bool condition = true;
-    for( ; i < 5; )
-    {
-        std::cout << "hello world" << std::endl;
-        i++;
-        if(i >= 5)
-        {
-            condition = false;
-        }
-    }
+    int var = 8;
+    int* ptr = &var; // (type)* => pointer. A pointer is a integer which hold the memory address of an value;
+                    // &(type) => get pointer of a value;
+    *ptr = 10;      // *pointer => get actual value stored in that memory address which pointer is pointing.
+                    // type of pointer is actually says nothing about pointer's type. A pointer will always be an integer.
+                    // we are using that type to tell the compiler how many bytes should a value take.
+                    // so we can re assign or modify a pointers value. For example if I didn't write int* in the example 
+                    // how can we re assign it. 
+    std::cout << var << std::endl;
     
 }
