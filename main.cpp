8673eb79be9fca14274bdfd94a0e2097bdc20441
amedduman <amedduman@protonmail.com>
2022-11-18 01:51:20 +0300

    Add class

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 
+class Player
+{
+public:
+    int speed;
+    int health;
+
+    void IncreaseHealth(int plusHealth)
+    {
+        health += plusHealth;
+    }
+};
+
 int main()
 {
-    int a = 8;
-    int b = 10;
-
-    int& ref = a;
-    ref = b; // you cannot assign a ref to reference another variable.
-
-    //the code above will modify value of a.
-
-    // we will end up with a = 10 b = 10
-
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    
+    Player player;
+    player.speed = 4;
+    player.health = 5;
+    player.IncreaseHealth(5);
+    std::cout << player.health << std::endl; 
 }
