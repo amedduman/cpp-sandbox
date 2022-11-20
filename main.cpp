58eea6e4410491157bbe0d90fff8a59546411683
amedduman <amedduman@protonmail.com>
2022-11-20 23:17:38 +0300

    Static example

--- main.cpp
+++ main.cpp
@@ -1,39 +1,8 @@
 #include <iostream>
 
-class Log
-{
-public:
-    int ErrorOnly = 0;
-    int WarnAndError = 1;
-    int All = 2;
-private:
-    int m_LogLevel = All;
-public:
-    void SetLogFilter(int filter)
-    {
-        m_LogLevel = filter;
-    }
-    void LogError(std::string message)
-    {
-        if(m_LogLevel >= ErrorOnly)
-            std::cout << "[ERROR]: " << message << std::endl;
-    }
-    void LogWarning(std::string message)
-    {
-        if(m_LogLevel >= WarnAndError)
-            std::cout << "[WARN]: " << message << std::endl;
-    }
-    void LogInfo(std::string message)
-    {
-        if(m_LogLevel >= All)
-            std::cout << "[INFO]: " << message << std::endl;
-    }
-};
+extern int s_num;
 
 int main()
 {
-   Log log;
-   log.SetLogFilter(log.All);
-   log.LogError("error");
-   log.LogInfo("hello");
+    std::cout << s_num << std::endl;
 }
---
+++ staticExample.cpp
@@ -0,0 +1,1 @@
+static int s_num = 5;
