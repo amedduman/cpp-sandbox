e347a40eb583609644f928a7ddf20cfa1194cb58
amedduman <amedduman@protonmail.com>
2022-12-26 14:17:40 +0300

    for loops

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
-
-void changeNum(int* num)
-{
-    *num = 5;
-}
+#include <vector>
 
 int main()
 {
-    int num = 3;
-    int* numPtr = &num;
-    changeNum(numPtr);
-    std::cout << num << std::endl;
+    std::vector<int> numbers;
+    numbers.push_back(1);
+    numbers.push_back(2);
+
+    for (size_t i = 0; i < numbers.size(); ++i)
+    {
+        if(i == 0)
+        {
+            std::cout << "number is one" << std::endl;
+        }
+        std::cout << numbers[i] << std::endl;
+    }
+    std::cout << "-----------------" << std::endl;
+    for (auto a : numbers)
+    {
+        std::cout << a << std::endl;
+    }
 }
