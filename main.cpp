2f21852ac4ebbedb9c2be6dd3290ed00f0e16eb2
amedduman <amedduman@protonmail.com>
2022-12-26 14:48:15 +0300

    range based for loop

--- main.cpp
+++ main.cpp
@@ -3,20 +3,12 @@
 
 int main()
 {
-    std::vector<int> numbers;
-    numbers.push_back(1);
-    numbers.push_back(2);
+    std::vector<float> numbers;
+    numbers.push_back(1.2f);
+    numbers.push_back(2.3f);
 
-    for (size_t i = 0; i < numbers.size(); ++i)
-    {
-        if(i == 0)
-        {
-            std::cout << "number is one" << std::endl;
-        }
-        std::cout << numbers[i] << std::endl;
-    }
     std::cout << "-----------------" << std::endl;
-    for (auto a : numbers)
+    for (auto& a : numbers)
     {
         std::cout << a << std::endl;
     }
