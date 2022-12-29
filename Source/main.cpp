8628833206ec6232a73bf2ab0d4f0a86d4aed343
amedduman <amedduman@protonmail.com>
2022-12-30 01:28:34 +0300

    pointers

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+void pp(int& i)
+{
+    std::cout << &i << " " << i << " "<< sizeof(i) << std::endl;
+}
+
 int main()
 {
-    
+    int arr[10] = {};
+    int* harr = new int[10];
+
+    for(auto& i:arr)
+    {
+        pp(i);
+    }
+
+    std::cout << "-----------------------" << std::endl;
+
+    for(auto i = 0; i < 10; i++)
+    {
+        pp(harr[i]);
+    }
 }
