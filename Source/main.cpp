33bb79b944cca428877daca75f694b22f17b5d48
amedduman <amedduman@protonmail.com>
2022-12-30 17:06:23 +0300

    Add sfml

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,58 +1,25 @@
-#include <iostream>
-
-void pp(int& i)
-{
-    std::cout << &i << " " << i << " "<< sizeof(i) << std::endl;
-}
-
-template <typename T>
-class DynamicArray
-{
-    T* m_arr;
-    size_t m_size;
-public:
-    DynamicArray(size_t size)
-        : m_size(size), m_arr(new T[size]) // when we create a array with new operator it will set all values to its default .
-    {
-        std::cout << "array constructed" << std::endl;
-    }
-
-    ~DynamicArray()
-    {
-        delete [] m_arr;
-        std::cout << "array destructed" << std::endl;
-    }
-
-    int get(size_t index) const
-    {
-        return m_arr[index];
-    }
-
-    void set(size_t index, T val)
-    {
-        m_arr[index] = val;
-    }
-
-    void print() const
-    {
-        for(size_t i = 0; i < m_size; i++)
-        {
-            std::cout << i << " => " << m_arr[i] << std::endl;
-        }
-    }
-
-    T& operator [](size_t index)
-    {
-        return m_arr[index];
-    }
-};
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    DynamicArray<float> myArr(10);
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+    shape.setFillColor(sf::Color::Cyan);
 
-    myArr[2] = 3.2f;
+    while (window.isOpen())
+    {
+        sf::Event event;
 
-    myArr.print();
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
