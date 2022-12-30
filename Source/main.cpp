b225ce5ff51793fbb7ea2c4ae0a4304c817ab244
amedduman <amedduman@protonmail.com>
2022-12-30 16:45:33 +0300

    make our own array

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,14 +5,54 @@ void pp(int& i)
     std::cout << &i << " " << i << " "<< sizeof(i) << std::endl;
 }
 
+template <typename T>
+class DynamicArray
+{
+    T* m_arr;
+    size_t m_size;
+public:
+    DynamicArray(size_t size)
+        : m_size(size), m_arr(new T[size]) // when we create a array with new operator it will set all values to its default .
+    {
+        std::cout << "array constructed" << std::endl;
+    }
+
+    ~DynamicArray()
+    {
+        delete [] m_arr;
+        std::cout << "array destructed" << std::endl;
+    }
+
+    int get(size_t index) const
+    {
+        return m_arr[index];
+    }
+
+    void set(size_t index, T val)
+    {
+        m_arr[index] = val;
+    }
+
+    void print() const
+    {
+        for(size_t i = 0; i < m_size; i++)
+        {
+            std::cout << i << " => " << m_arr[i] << std::endl;
+        }
+    }
+
+    T& operator [](size_t index)
+    {
+        return m_arr[index];
+    }
+};
+
 int main()
 {
-    int a = 1;
-    int b = 2;
+    DynamicArray<float> myArr(10);
 
-    // we modify a without accessing it directly. This is an example why pointers are unsafe.
-    *(&b + 1) = 5;
+    myArr[2] = 3.2f;
 
-    pp(a);
-    pp(b);
+    myArr.print();
+
 }
